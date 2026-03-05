3f7cddd173e59933929fd7beafb812522171f2c0
Skylar Koningin <greekie.via.linux@gmail.com>
2026-03-05 09:04:07 -0500

    updated license headers and the build script

--- NonArbitrary_Precision/Rust/src/cc_extras.rs
+++ NonArbitrary_Precision/Rust/src/cc_extras.rs
@@ -2,13 +2,15 @@
  * SPDX-License-Identifier: Apache-2.0 OR MPL-2.0
  *
  * This Source Code Form is subject to the terms of the Apache License,
- * Version 2.0. If a copy of the Apache License, Version 2.0 was not
- * distributed with this file, You can obtain one at
- * http://www.apache.org/licenses/LICENSE-2.0
+ * v. 2.0. If a copy of the Apache License was not distributed with this
+ * file, You can obtain one at http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Alternatively, the contents of this file may be used under the terms
- * of the Mozilla Public License, v. 2.0, in which case the provisions of
- * the MPL are applicable instead of those above.
+ * Alternatively, this Source Code Form is subject to the terms of the 
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+ * distributed with this file, You can obtain one at 
+ * https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2026 Skylar Koningin
  */
 
 use std::fs;
--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-#
-# Any copyright is dedicated to the Public Domain.
-# https://creativecommons.org/publicdomain/zero/1.0/
-#
 # SPDX-License-Identifier: CC0-1.0
 #
+# Written in 2026 by Skylar Koningin
+#
+# To the extent possible under law, the author(s) have dedicated all 
+# copyright and related and neighboring rights to this software to 
+# the public domain worldwide. This software is distributed without 
+# any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication 
+# along with this software. If not, see:
+# http://creativecommons.org/publicdomain/zero/1.0/
 
 # Script Dependency Check
 package_exists() {
@@ -47,7 +53,9 @@ cd ../..
 # CSharp Compilation
 cd Arbitrary_Precision/CSharp/
 dotnet publish --sc -o ../../build/CSharp -p:PublishSingleFile=true
+rm -r bin obj
 cd ../../build/CSharp
+rm *.pdb
 cd ../..
 
 # Go Compilation
