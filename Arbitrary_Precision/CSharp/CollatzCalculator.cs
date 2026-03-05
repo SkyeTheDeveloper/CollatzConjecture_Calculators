5776c9df4312d8bb0a011c1479e4896b0cbd3965
Skylar Koningin <greekie.via.linux@gmail.com>
2026-03-05 10:04:50 -0500

    idk why it didnt update these

--- Arbitrary_Precision/CSharp/CollatzCalculator.cs
+++ Arbitrary_Precision/CSharp/CollatzCalculator.cs
@@ -1,15 +1,17 @@
-﻿// SPDX-License-Identifier: Apache-2.0 OR MPL-2.0
+﻿// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
 //
 // This Source Code Form is subject to the terms of the Apache License,
-// v. 2.0. If a copy of the Apache License was not distributed with this
-// file, You can obtain one at http://www.apache.org/licenses/LICENSE-2.0.
+// v. 2.0, or the GNU Lesser General Public License, v. 3.0 or later.
+// If a copy of the Apache License was not distributed with this file,
+// You can obtain one at http://www.apache.org/licenses/LICENSE-2.0.
 //
-// Alternatively, this Source Code Form is subject to the terms of the 
-// Mozilla Public License, v. 2.0. If a copy of the MPL was not 
-// distributed with this file, You can obtain one at 
-// https://mozilla.org/MPL/2.0/.
+// Alternatively, this Source Code Form is subject to the terms of the
+// GNU Lesser General Public License, v. 3.0 or later. If a copy of the
+// LGPL was not distributed with this file, You can obtain one at
+// https://www.gnu.org/licenses/lgpl-3.0.html.
 //
 // Copyright 2026 Skylar Koningin
+
 
 using System.Numerics;
 using CollatzCalculatorExtras;
